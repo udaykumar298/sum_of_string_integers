8fc662abb2727c90c7d2938567b1090a28903a71
udaykumar298 <v.udaykumar222@gmail.com>
2025-04-10 00:22:38 +0530

    Added more test cases -Added test cases as per the requirement (currently failing since logic isn't implemented)

--- test/test_sum_of_integers_in_string.rb
+++ test/test_sum_of_integers_in_string.rb
@@ -1,5 +1,7 @@
 require 'minitest/autorun'
+require 'minitest/reporters'
 require_relative '../sum_of_integers_in_string'
+Minitest::Reporters.use!(Minitest::Reporters::SpecReporter.new)
 
 class TestSumOfIntegersInString < Minitest::Test
 
@@ -18,4 +20,29 @@ class TestSumOfIntegersInString < Minitest::Test
     assert_equal 1, SumOfIntegersInString.add("a1")
   end
 
+  def test_single_number
+    assert_equal 5, SumOfIntegersInString.add("5")
+  end
+
+  def test_multiple_numbers
+    assert_equal 15, SumOfIntegersInString.add("1,2,3,4,5")
+  end
+
+  def test_numbers_with_text
+    assert_equal 6, SumOfIntegersInString.add("abc1xyz2lmn3")
+  end
+
+  def test_numbers_with_newlines_and_commas
+    assert_equal 10, SumOfIntegersInString.add("1\n2,3\n4")
+  end
+
+  def test_negative_number_raises_exception
+    error = assert_raises(RuntimeError) { SumOfIntegersInString.add("1,-2,3") }
+    assert_equal "negative numbers not allowed -2", error.message
+  end
+
+  def test_multiple_negative_numbers_raise_exception
+    error = assert_raises(RuntimeError) { SumOfIntegersInString.add("1,-2,-3") }
+    assert_equal "negative numbers not allowed -2,-3", error.message
+  end
 end

080e73282a8d1ffb9f29790f750476c6575d533c
udaykumar298 <v.udaykumar222@gmail.com>
2025-04-10 00:39:24 +0530

    Added custom exception -Implemented logic to raise a custom exception

--- sum_of_integers_in_string.rb
+++ sum_of_integers_in_string.rb
@@ -1,5 +1,12 @@
 class SumOfIntegersInString
   def self.add(numbers)
-    numbers.scan(/\d+/).map(&:to_i).sum
+    nums = numbers.scan(/-?\d+/).map(&:to_i)
+    negatives = nums.select { |n| n < 0 }
+
+    if negatives.any?
+      raise "negative numbers not allowed #{negatives.join(',')}"
+    end
+
+    nums.sum
   end
 end
